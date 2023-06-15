5f3ac2a22fab97611f11b00c246399286925b92e
Manish <manish@delimp.com>
2023-06-15 15:24:58 +0530

    second commit

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Sdsd")
+
+//Arrays
+
+let color=["red","blue","green"]
+
+
+
+// varuable and block scope
+
+
+//Loops
+
+
+
+//Functions 
+
+/*
+function nameofFunction(){
+    console.log("Fdfd")
+}
+
+nameofFunction()
+*/
+//function expersion 
+
+let  
